1fe602e8b878ffb697aac2d900c72a1ea41a7ad1
brianmachado18 <andrs.machado1@gmail.com>
2025-05-22 19:32:20 -0300

    cerrar sesion

--- src/pages/CerrarSesion.jsx
+++ src/pages/CerrarSesion.jsx
@@ -4,24 +4,30 @@ import NavbarVendedor from "../components/NavbarVendedor";
 import "../css/CerrarSesion.css";
 
 function CerrarSesion() {
+
+  function cerrarSesion() {
+    localStorage.removeItem("token");
+    window.location.href = "/login";
+  }
+
   return (
     <>
-    <div className="cerrarSesion-bg">
-      <div className="cerrarSesion-card card p-4 shadow-lg">
-        
-        <div className="mb-3">
+      <div className="cerrarSesion-bg">
+        <div className="cerrarSesion-card card p-4 shadow-lg">
+
+          <div className="mb-3">
             <h3>Esta seguro que quiere cerrar sesion?</h3>
-        </div>
-        <div class="d-grid gap-2">
+          </div>
+          <div class="d-grid gap-2">
             <Link className="nav-link" to="/login">
-                <button className="btn w50 btn-primary rounded-pill">Cerrar</button>
+              <button className="btn w50 btn-primary rounded-pill">Cerrar</button>
             </Link>
             <Link className="nav-link" to="/">
-                <button className="btn w50 btn-secondary rounded-pill">Cancelar</button>
+              <button className="btn w50 btn-secondary rounded-pill" onClick={cerrarSesion}>Cancelar</button>
             </Link>
+          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
