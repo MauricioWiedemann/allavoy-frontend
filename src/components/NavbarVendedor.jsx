0529787200b00f8e72566bdac89e8168a30a6d3c
brianmachado18 <andrs.machado1@gmail.com>
2025-05-26 17:51:39 -0300

    menu usuarios

--- src/components/NavbarVendedor.jsx
+++ src/components/NavbarVendedor.jsx
@@ -1,48 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 import logo from '../../sources/logo.png';
 import "../css/Navbar.css";
 
 function NavbarVendedor() {
+  const [showGestion, setShowGestion] = useState(false);
+  const [showPasajes, setShowPasajes] = useState(false);
+  const [showUsuario, setShowUsuario] = useState(false);
+
   return (
-    <aside class="sidebar">
-      <div class="sidebar-header">
-        <img src={logo} alt="Logo"/>
+    <aside className="sidebar">
+      <div className="sidebar-header">
+        <img src={logo} alt="Logo" />
         <h2>AllaVoy</h2>
       </div>
-      <ul class="navbar-nav ms-auto">
-        <li className="nav-item">
-          <Link className="nav-link" to="/">Inicio</Link>
+      <ul className="navbar-nav ms-auto">
+        <li>
+          <div className="menu-title" onClick={() => setShowGestion(!showGestion)}>
+            <h4>Gestión</h4>
+            {showGestion ? <FaChevronUp /> : <FaChevronDown />}
+          </div>
+          {showGestion && (
+            <>
+              <li className="nav-item"> <Link className="nav-link" to="/altalocalidad">Alta Localidad</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/altaomnibus">Alta Omnibus</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/altaviaje">Alta Viaje</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/">Reasignación de viaje</Link></li>
+            </>
+          )}
         </li>
-        <h4>Gestio</h4>
-        <li className="nav-item">
-          <Link className="nav-link" to="/altalocalidad">Alta Localidad</Link>
+        <li>
+          <div className="menu-title" onClick={() => setShowPasajes(!showPasajes)}>
+            <h4>Pasajes</h4>
+            {showPasajes ? <FaChevronUp /> : <FaChevronDown />}
+          </div>
+          {showPasajes && (
+            <>
+              <li className="nav-item"><Link className="nav-link" to="/">Compra</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/">Cierre de venta</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/">Listado de ventas</Link></li>
+            </>
+          )}
         </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/altaomnibus">Alta Omnibus</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/altaviaje">Alta Viaje</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/">Reasignacion de viaje</Link>
-        </li>
-        <h4>Pasajes</h4>
-        <li className="nav-item">
-          <Link className="nav-link" to="/">Compra</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/">Cierre de venta</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/">Listado de ventas</Link>
-        </li>
-        <h4>Usuario</h4>
-        <li className="nav-item">
-          <Link className="nav-link" to="/">Soporte</Link>
-        </li>
-        <li className="nav-item">
-          <Link className="nav-link" to="/cerrarsesion">Cerrar Sesion</Link>
+        <li>
+          <div className="menu-title" onClick={() => setShowUsuario(!showUsuario)}>
+            <h4>Usuario</h4>
+            {showUsuario ? <FaChevronUp /> : <FaChevronDown />}
+          </div>
+          {showUsuario && (
+            <>
+              <li className="nav-item"><Link className="nav-link" to="/">Soporte</Link></li>
+              <li className="nav-item"><Link className="nav-link" to="/cerrarsesion">Cerrar Sesión</Link></li>
+            </>
+          )}
         </li>
       </ul>
     </aside>
