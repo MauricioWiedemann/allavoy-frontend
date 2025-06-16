22219cf1a6afecb657988fbe7ebc7e147761b73c
MauricioWiedemann <m.wiedemanngarcia@gmail.com>
2025-06-16 20:52:16 -0300

    Update cerrar sesion y lista pasajes

--- src/pages/Raiz.jsx
+++ src/pages/Raiz.jsx
@@ -6,12 +6,7 @@ export default function Raiz() {
   function validarTokenUsuario(){
     try {
       let payload = jwtDecode(localStorage.getItem("token"));
-      if (payload.rol === "CLIENTE")
-        window.location.href = "/homec";
-      else if (payload.rol === "VENDEDOR")
-        window.location.href = "/homev";
-      else
-        window.location.href = "/homea";
+      window.location.href = "/home";
     } catch (e) {
       window.location.href = "/login";
     }
